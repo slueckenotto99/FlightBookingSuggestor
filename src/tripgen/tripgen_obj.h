93e55eb7b4e3c2a5b1b7e0525ed94449a0ed8232
slueckenotto99 <slueckenotto99@gmail.com>
2024-03-23 14:32:18 -0400

    Initial project setup

---
+++ src/tripgen/tripgen_obj.h
@@ -0,0 +1,25 @@
+/*
+
+    Trip Itinerary Generation Object Header File
+
+*/
+
+#ifndef __TRIPGEN_OBJ__
+#define __TRIPGEN_OBJ__
+
+
+
+class tripgen_obj_t
+{
+    public:
+
+
+    protected:
+
+
+    private:
+
+
+};
+
+#endif
